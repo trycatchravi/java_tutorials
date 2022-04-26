0bdd6b08bc182e6c3007f0bab69724f8e8789610
Narender <narenderb18@gmail.com>
2022-04-26 11:47:54 +0530

    Wap to find if a string has all unique character

--- src/com/basics/UniqueCharactersInString.java
+++ src/com/basics/UniqueCharactersInString.java
@@ -2,17 +2,29 @@ package com.basics;
 
 public class UniqueCharactersInString {
 
-    public static boolean findIfStringHasUniqueCharacters(String s) {
-        // write the logic here
+    public static boolean findIfStringHasUniqueCharacters(String string) {
+
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (string.indexOf(ch) != string.lastIndexOf(ch)) {
+                return false;
+            }
+        }
         return true;
     }
 
     public static void main(String[] args) {
-       boolean unique = findIfStringHasUniqueCharacters("absbabd");
-       System.out.println(unique); // false
+        boolean unique= findIfStringHasUniqueCharacters("narender");
+        System.out.println("narender has all unique character : "+ unique);
 
-        unique = findIfStringHasUniqueCharacters("abcdef");
-        System.out.println(unique); // true
+        unique=findIfStringHasUniqueCharacters("index");
+        System.out.println("index has all unique character : "+ unique);
+
+        unique=findIfStringHasUniqueCharacters("apple");
+        System.out.println("apple has all unique character : "+ unique);
+
+        unique=findIfStringHasUniqueCharacters("world");
+        System.out.println("world has all unique character : "+ unique);
     }
 
 }
